e96fb335d7351a3398a78710b92df65c7a7286ff
Kognise <felix.mattick@gmail.com>
2019-06-02 13:34:18 -0400

    Remove update script and hide aside

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -35,9 +35,9 @@ export default withRouter((props) => {
           Learn more
         </SmallButton>
       </Aside>
-      <Aside>
+      {/* <Aside>
         <strong style={{ fontWeight: 500 }}>Do not enter your Google login info here! This is a fake login screen and was built for educational purposes.</strong>
-      </Aside>
+      </Aside> */}
 
       <Actions>
         <SecondaryAction onClick={() => {
